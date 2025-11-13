3b8fa7e04e241e97a9d47c64ce1f92fd02401bfe
aslı doğan <a.dgn2003@gmail.com>
2025-11-13 19:28:07 +0300

    [NO-1] - temel oluşturuldu

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,7 +1,24 @@
 import Welcome from "@components/welcome/welcome";
+import LoginButton from "@components/welcome/LoginButton/LoginButton";
 
 function Home() {
   return <Welcome />;
 }
+
+export default Home;
+
+
+
+const Home = () => {
+  const handleLogin = () => {
+    alert("Login clicked!");
+  };
+
+  return (
+    <div>
+      <LoginButton onClick={handleLogin} />
+    </div>
+  );
+};
 
 export default Home;
