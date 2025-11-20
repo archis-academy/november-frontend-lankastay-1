c66a9e3de13cd991fc70fd71ad2df4f48c380cc1
Coders Club <cod@gmail.com>
2025-11-20 23:13:59 +0300

    quick stats component içindeki karışık kodlar silindi.

--- src/pages/playground/index.jsx
+++ src/pages/playground/index.jsx
@@ -1,16 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import QuickStats, {
-  CameraIcon,
-  TravellerIcon,
-  LocationIcon,
-} from '../../components/quick-stats-component/quick_stats';
+import QuickStats from '../../components/quick-stats-component/quick_stats';
 import fetchData from '../../lib/fetchData';
-
-const ICONS = {
-  camera: CameraIcon,
-  traveller: TravellerIcon,
-  location: LocationIcon,
-};
 
 const Playground = () => {
   const [quickIcons, setQuickIcons] = useState([]);
@@ -22,11 +12,9 @@ const Playground = () => {
   return (
     <div style={{display:"flex"}}>
       {quickIcons.map((item) => {
-        const Icon = ICONS[item.iconKey] ; // db.json'da iconKey yoksa hep CameraIcon gelir
-
         return (
           <div key={item.id}>
-            <QuickStats number={item.number} content={item.content} Icon={Icon} />
+            <QuickStats number={item.number} content={item.content} iconUrl={item.iconUrl} />
           </div>
         );
       })}
