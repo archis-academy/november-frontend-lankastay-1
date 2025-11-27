6faa3f5c632cf064aedce270558601268e8bb5be
MuratEetugral <muratertugral93@gmail.com>
2025-11-27 20:35:06 +0300

    Most picked grid section added

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import HeroSection from '../../components/heroSection';
+import MostPickedSection from '../../sections/mostPickedSection';
 
 
 export const Home = () => {
-  return <div><HeroSection /></div>;
+  return <div>
+    <HeroSection />
+    <MostPickedSection />
+  </div>;
 };
