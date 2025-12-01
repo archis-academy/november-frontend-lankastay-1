43d47efff4607ee041baf9a5d4121511a194898f
Coders Club <cod@gmail.com>
2025-12-01 23:11:18 +0300

    input component done

--- src/pages/playground/index.jsx
+++ src/pages/playground/index.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import fetchData from '../../lib/fetchData.js';
 import ChoiseCard from '../../Components/ChoiseCard/ChoiseCard';
-import QuickStats from "../../Components/quick-stats-component/quick_stats"
+import QuickStats from '../../Components/quick-stats-component/quick_stats';
+import Input from '../../Components/inputComponent/input.jsx';
+import { data } from 'react-router-dom';
 
 const Playground = () => {
   const [quickIcons, setQuickIcons] = useState([]);
   const [cards, setCards] = useState([]);
+  const [inputs, setInputs] = useState([]);
 
   useEffect(() => {
     fetchData('quickStats').then((data) => setQuickIcons(data));
     fetchData('choise-cards').then((data) => setCards(data));
+    fetchData('createAccountUser').then((data) => setInputs(data));
   }, []);
+
+  useEffect(() => {}, []);
 
   return (
     <div>
@@ -27,7 +33,6 @@ const Playground = () => {
         />
       ))}
 
-     
       <div style={{ display: 'flex' }}>
         {quickIcons.map((item) => {
           return (
@@ -37,7 +42,23 @@ const Playground = () => {
           );
         })}
       </div>
-   
+
+      <form>
+        {inputs.map((item) => {
+          return (
+            <div>
+              <Input
+                label={item.label}
+                placeholder={item.placeholder}
+                type={item.type}
+                key={item.id}
+              />
+            </div>
+          );
+        })}
+      </form>
+
+      <div></div>
     </div>
   );
 };
