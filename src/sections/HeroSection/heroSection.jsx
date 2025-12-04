63a8ccbcdafbc8d4f62984e4bc57b609fc53245e
MuratEetugral <muratertugral93@gmail.com>
2025-12-04 20:45:13 +0300

    feat: implement dynamic quick stats fetching and update button component

--- src/sections/HeroSection/heroSection.jsx
+++ src/sections/HeroSection/heroSection.jsx
@@ -1,44 +1,46 @@
-import React from 'react';
-import styles from './heroSection.module.scss'; 
+import React, { useEffect, useState } from 'react';
+import styles from './heroSection.module.scss';
 import QuickStats from '../../Components/quick-stats-component/quick_stats';
+import Button from '../../Components/Button/Button';
+import fetchData from '../../lib/fetchData';
 
 const HeroSection = () => {
+  const [quickStatsData, setQuickStatsData] = useState([]);
+
+  useEffect(() => {
+    fetchData('quickStats').then((data) => setQuickStatsData(data));
+  }, []);
   return (
     <section className={styles['hero-section']}>
       <div className={styles['hero-container']}>
-    
         <div className={styles['hero-content']}>
           <h1 className={styles['hero-title']}>
             Forget Busy Work, <br />
             Start Next Vacation
           </h1>
-          
+
           <p className={styles['hero-description']}>
-            We provide what you need to enjoy your holiday with family. 
-            Time to make another memorable moments.
+            We provide what you need to enjoy your holiday with family. Time to make another
+            memorable moments.
           </p>
 
-          <button className={styles['btn-primary']}>
-            Show More
-          </button>
+          <Button text='Show More' />
 
-          <Button  />
 
           <div className={styles['hero-stats-wrapper']}>
-            <QuickStats iconUrl="/icons/icon1.svg" number="2500" content="Users" />
-            <QuickStats iconUrl="/icons/icon2.svg" number="200" content="Treasure" />
-            <QuickStats iconUrl="/icons/icon3.svg" number="100" content="Cities" />
+            {quickStatsData.map((item) => {
+              return (
+                <div key={item.id}>
+                  <QuickStats number={item.number} content={item.content} iconUrl={item.iconUrl} />
+                </div>
+              );
+            })}
           </div>
         </div>
 
         <div className={styles['hero-image-wrapper']}>
-          <img 
-            src="/images/banner.svg" 
-            alt="Vacation Home" 
-            className={styles['hero-image']}
-          />
+          <img src='/images/banner.svg' alt='Vacation Home' className={styles['hero-image']} />
         </div>
-
       </div>
     </section>
   );
