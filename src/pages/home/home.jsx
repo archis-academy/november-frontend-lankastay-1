fef5ee6e8e7cb2a2df658ae38b60fc88b8c18d82
MuratEetugral <muratertugral93@gmail.com>
2025-12-11 21:08:15 +0300

    Add HeroSection component to Home page

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import Header from '../../Components/Header/Header';
 import SearchFilterBar from '../../Components/SearchFilterBar/SearchFilterBar';
 import Footer from '../../Components/footer/Footer';
+import HeroSection from '../../sections/HeroSection/heroSection'
+
 
 export const Home = () => {
   return (
     <div>
       <Header isShort={true} />
+      <HeroSection />
       <SearchFilterBar />
       <Footer />
     </div>
