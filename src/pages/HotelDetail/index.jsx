936e65b6ea4dcb182a3ad075832a6f66bc4bfb23
MuratEetugral <muratertugral93@gmail.com>
2025-12-15 20:31:02 +0300

    Add eslint comment to suppress warning for setAmenities in useEffect

--- src/pages/HotelDetail/index.jsx
+++ src/pages/HotelDetail/index.jsx
@@ -16,6 +16,7 @@ const HotelDetail = () => {
   }, []);
 
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     setAmenities(selectedHotel?.amenities);
   }, [selectedHotel]);
 
