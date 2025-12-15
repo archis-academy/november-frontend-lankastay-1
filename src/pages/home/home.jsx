0aac17503757cdc4fe9e8ab7e6af0096b44fe6b6
MuratEetugral <muratertugral93@gmail.com>
2025-12-15 20:29:59 +0300

    Refactor HotelDetail component and clean up Home component

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,33 +1,16 @@
-import React , { useState, useEffect } from 'react';
+import React from 'react';
 import Header from '../../Components/Header/Header';
 import SearchFilterBar from '../../Components/SearchFilterBar/SearchFilterBar';
 import Footer from '../../Components/footer/Footer';
-import HeroSection from '../../sections/HeroSection/heroSection'
-import Breadcrumb from '../../Components/Breadcrumb/breadcrumb';
-import ProductDescription from '../../Components/ProductDescription/ProductDescription';
-import Amentiti from '../../sections/mock/amentiti';
-import fetchData from '../../lib/fetchData';
+import HeroSection from '../../sections/HeroSection/heroSection';
 
 export const Home = () => {
-  const [amenities, setAmenities] = useState([]);
-
-  useEffect(() => {
-    fetchData('hotelDetails').then((data) => {
-      setAmenities(data[0]?.amenities);
-    });
-  }, []);
-
   return (
     <div>
-      
       <Header isShort={true} />
-      <Breadcrumb currentPage="Hotel Details"/>
-      <ProductDescription />
-      {amenities?.length > 0 && <Amentiti data={amenities} />}
       <HeroSection />
       <SearchFilterBar />
       <Footer />
-      
     </div>
   );
 };
