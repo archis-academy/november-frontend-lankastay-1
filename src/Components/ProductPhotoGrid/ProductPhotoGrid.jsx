1807bff1ffd36d49acf380ab8e3a5778afdf9292
aslı doğan <a.dgn2003@gmail.com>
2025-12-18 18:44:09 +0300

    [NO-20] - .

--- src/Components/ProductPhotoGrid/ProductPhotoGrid.jsx
+++ src/Components/ProductPhotoGrid/ProductPhotoGrid.jsx
@@ -6,19 +6,19 @@ const ImageGrid = ({images}) => {
     <div className="image-grid">
       <div className="image-grid__main">
         <img
-          src={images && images.lenght > 0 ? images[0] : '/images/ProductPhotoGrid/Product1.png'}
+          src={images && images.length > 0 ? images[0] : '/images/ProductPhotoGrid/Product1.png'}
           alt="House"
           className="image-grid__main-image"
         />
       </div>
       <div className="image-grid__side">
         <img
-          src={images && images.lenght > 1 ? images[1] : '/images/ProductPhotoGrid/Product2.png'}
+          src={images && images.length > 1 ? images[1] : '/images/ProductPhotoGrid/Product2.png'}
           alt="Bedroom"
           className="image-grid__side-image"
         />
         <img
-         src={images && images.lenght > 2 ? images[2] : '/images/ProductPhotoGrid/Product3.png'}
+         src={images && images.length > 2 ? images[2] : '/images/ProductPhotoGrid/Product3.png'}
           alt="Bathroom"
           className="image-grid__side-image"
         />
