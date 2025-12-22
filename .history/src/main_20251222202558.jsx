ee8a1f8cebaa810dae7c54add998659184fcc0d8
Beritan Bulgan <berixanim@gmail.com>
2025-12-22 20:32:10 +0300

    [NO-26] Modal Component

---
+++ .history/src/main_20251222202558.jsx
@@ -0,0 +1,36 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home } from './pages/home/home';
+import '@/main.scss';
+import Playground from './pages/playground';
+import HotelDetail from './pages/HotelDetail';
+import Breadcrumb from './Components/Breadcrumb/breadcrumb';
+import Login from './Components/Login/Login';
+import RegisterAdmin from './pages/RegisterAdmin/RegisterAdmin';
+import SuccesPage from './pages/SuccesRegister/SuccesPage';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/playground' element={<Playground />} />
+        <Route
+          path='/login'
+          element={<Login dataType={'loginAccount'} dataTypeDetail={'loginAccountDetail'} />}
+        ></Route>
+        <Route
+          path='/register'
+          element={
+            <Login dataType={'createAccountUser'} dataTypeDetail={'registerAccountDetail'} />
+          }
+        ></Route>
+        <Route path='/admin-register' element={<RegisterAdmin />}></Route>
+        <Route path='/hotel-detail/:id' element={<HotelDetail />} />
+        <Route path='/breadcrumb/:id' element={<Breadcrumb />} />
+        <Route path='/success-register' element={<SuccesPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
