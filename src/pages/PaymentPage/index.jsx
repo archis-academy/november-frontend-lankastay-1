e701b4117cfcdd6875ad99662d8f3bdaf4b5ba60
aslı doğan <a.dgn2003@gmail.com>
2025-12-23 20:40:57 +0300

    [NO-41] - ..

--- src/pages/PaymentPage/index.jsx
+++ src/pages/PaymentPage/index.jsx
@@ -1,7 +1,33 @@
+import React from "react";
+import Header from "../../Components/Header/Header";
+import StepIndicator from "../../Components/StepIndicator/StepIndicator";
 import PaymentScreen from "../../Components/PaymentScreen/PaymentScreen";
+import Footer from "../../Components/Footer/Footer";
+import styles from "./PaymentPage.module.scss";
+
 
 const PaymentPage = () => {
-  return <PaymentScreen />;
+  return (
+    <>
+      <header className={styles.paymentHeader}>
+        <img
+          src="/logo.svg"
+          alt="Logo"
+          className={styles.headerLogo}
+        />
+      </header>
+
+      <section className={styles.stepWrapper}>
+        <StepIndicator currentStep={2} />
+      </section>
+
+      <main className={styles.main}>
+        <PaymentScreen />
+      </main>
+
+      <Footer />
+    </>
+  );
 };
 
 export default PaymentPage;
