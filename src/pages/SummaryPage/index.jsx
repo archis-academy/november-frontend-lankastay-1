36c09425ea4f92f1073edc38624428cbe01a4c9b
esrask319 <esrask319@icloud.com>
2025-12-23 10:55:49 +0300

    jsx kodlarında duzenlemeler yapıldı

---
+++ src/pages/SummaryPage/index.jsx
@@ -0,0 +1,39 @@
+import React, {useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+import fetchData from '../../lib/fetchData';
+import LocationSummary from '../../Components/LocationSummary/LocationSummary';
+import styles from './SummaryPage.module.scss';
+
+const SummaryPage = () => {
+  const [hotelDetails, setHotelDetails] = useState([]);
+    const { id } = useParams();
+
+    useEffect(() => {
+        fetchData('hotelDetail').then((data) => setHotelDetails(data));
+    }, []);
+
+    const selectedHotel = hotelDetails?.find((hotel) => hotel?.id == id);
+
+  return (
+    <div className={styles.pageWrapper}>
+           <div className={styles.container}>
+                <div className={styles.contentWrapper}>
+                    
+                    {selectedHotel && (
+                        <>
+                            
+                            <LocationSummary 
+                                image={selectedHotel.images?.[0]} 
+                                locationName={selectedHotel.title} 
+                                location={selectedHotel.subtitle} 
+                            />
+                        </>
+                    )}
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SummaryPage;
