79ee7f548db72c5df95b5a2b3f65aaa95c0d3652
1aslidogan <email@example.com>
2026-01-08 02:07:18 +0300

    [NO-45] -..

--- src/Components/Booking/bookingCard.jsx
+++ src/Components/Booking/bookingCard.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./bookingCard.scss";
+import { Link } from "react-router-dom";
 
-const BookingCard = ({price, currency}) => {
+const BookingCard = ({price, currency , id}) => {
   return (
     <div className="booking-card">
       <h3 className="booking-card__title">Start Booking</h3>
@@ -11,10 +12,13 @@ const BookingCard = ({price, currency}) => {
         <span className="period">per Night</span>
       </div>
 
+<Link to={`/booking/${id}`} >
       <button className="booking-card__btn">
         Book Now
       </button>
+       </Link>
     </div>
+   
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import RegisterAdmin from './pages/RegisterAdmin/RegisterAdmin';
 import Dashboard from './pages/Dashboard';
 import SuccesPage from './pages/SuccesRegister/SuccesPage';
 import DashboardLayout from './pages/DashboardLayout/DashboardLayout.jsx';
-import BookingPage from './pages/BookingPage/BookingPage.jsx';
+import PaymentPage from './pages/PaymentPage/PaymentPage.jsx';
 import Setting from './pages/Settings/Setting.jsx';
 import Messages from './pages/Message/Message.jsx';
 import Booking from './pages/Bookings/Booking.jsx';
@@ -35,11 +35,11 @@ createRoot(document.getElementById('root')).render(
             <Login dataType={'createAccountUser'} dataTypeDetail={'registerAccountDetail'} />
           }
         ></Route>
-        <Route path='/admin-register' element={<RegisterAdmin />}></Route>
+        
         <Route path='/hotel-detail/:id' element={<HotelDetail />} />
-        <Route path='/breadcrumb/:id' element={<Breadcrumb />} />
+        <Route path='/booking/:id' element={<PaymentPage />}></Route>
         <Route path='/success-register' element={<SuccesPage />}></Route>
-        <Route path='/booking-page/:id' element={<BookingPage />}></Route>
+        <Route path='/admin-register' element={<RegisterAdmin />}></Route>
         <Route element={<DashboardLayout />}>
           <Route path='/Dashboard' element={<Dashboard />}></Route>
           <Route path='/Setting' element={<Setting />}></Route>
