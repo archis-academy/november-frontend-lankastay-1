28bc136e84949704b783d54f4fa522adacddbb73
Murat ERTUGRAL <muratertugral93@gmail.com>
2026-01-12 20:43:25 +0300

    Refactor dashboard layout and improve code readability

--- src/pages/DashboardLayout/DashboardLayout.jsx
+++ src/pages/DashboardLayout/DashboardLayout.jsx
@@ -1,12 +1,15 @@
 import DashboardSidebar from '../../Components/dashboardSidebar/DashboardSidebar';
 import { Outlet } from 'react-router-dom';
 import style from './DashboardLayout.module.scss';
+import DashboardNavbar from '../../sections/DashboardNavbar/DashboardNavbar';
 
 const DashboardLayout = () => (
   <div className={style.dashboardLayoutContainer}>
     
     <DashboardSidebar />
+
     <div style={{ flex: 1 }}>
+                <DashboardNavbar />
       <Outlet />
     </div>
   </div>
