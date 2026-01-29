3fc6d026736f81b0b8a9815cda8b4cee1c083100
Murat ERTUGRAL <muratertugral93@gmail.com>
2026-01-29 20:53:18 +0300

    vol5

--- src/sections/HeroSection/heroSection.jsx
+++ src/sections/HeroSection/heroSection.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import styles from './heroSection.module.scss';
-import QuickStats from '../../Components/quick-stats-component/quick_stats';
+
 import Button from '../../Components/Button/Button';
 import fetchData from '../../lib/fetchData';
+import QuickStats from '../../Components/quickStats';
 
 const HeroSection = () => {
   const [quickStatsData, setQuickStatsData] = useState([]);
